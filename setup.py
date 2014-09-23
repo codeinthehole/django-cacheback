55dfb87bf7cc9b02ab5f2944b5bfae4ba87d7fab
David Winterbottom <david.winterbottom@gmail.com>
2014-09-23 22:19:36 +0100

    Restrict version of Celery supported

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(name='django-cacheback',
       install_requires=[
           'django>=1.3,<1.7',
           'django-celery>=3.0',
+          'celery<3.2',
           ],
       # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Environment :: Web Environment',
