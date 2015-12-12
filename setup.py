370035b7b9103b7e2343213c3e869534036b6135
David Winterbottom <david.winterbottom@gmail.com>
2015-12-12 20:54:07 +0000

    Add new modifiers to setup.py

--- setup.py
+++ setup.py
@@ -8,27 +8,36 @@ PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
 os.chdir(PACKAGE_DIR)
 
 
-setup(name='django-cacheback',
-      version=__version__,
-      url='https://github.com/codeinthehole/django-cacheback',
-      author="David Winterbottom",
-      author_email="david.winterbottom@gmail.com",
-      description=("Caching library for Django that uses Celery "
-                   "to refresh cache items asynchronously"),
-      long_description=open(os.path.join(PACKAGE_DIR, 'README.rst')).read(),
-      license='MIT',
-      packages=find_packages(exclude=["sandbox*", "tests*"]),
-      include_package_data=True,
-      install_requires=[
-          'django>=1.3',
-          'celery<3.2',
-          'six',
-      ],
-      # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: Unix',
-                   'Programming Language :: Python']
-      )
+setup(
+    name='django-cacheback',
+    version=__version__,
+    url='https://github.com/codeinthehole/django-cacheback',
+    author="David Winterbottom",
+    author_email="david.winterbottom@gmail.com",
+    description=("Caching library for Django that uses Celery "
+                 "to refresh cache items asynchronously"),
+    long_description=open(os.path.join(PACKAGE_DIR, 'README.rst')).read(),
+    license='MIT',
+    packages=find_packages(exclude=["sandbox*", "tests*"]),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.3',
+        'celery',
+        'six',
+    ],
+    # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
