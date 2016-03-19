cd3e341561ba993308d786c5e5cc3fe0ec549d09
David Winterbottom <david.winterbottom@gmail.com>
2016-03-19 21:38:54 +0000

    Adjust provisioning of Vagrant box to install Celery

--- sandbox/provision.sh
+++ sandbox/provision.sh
@@ -5,7 +5,7 @@ apt-get install -y rabbitmq-server memcached python-pip git
 
 pip install -U pip
 cd /vagrant
-python setup.py develop
+pip install -e .[celery]
 `which pip` install -r requirements.txt -r sandbox/requirements.txt
 
 # Create database
