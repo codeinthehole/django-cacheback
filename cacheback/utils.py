88ff6404f03c982185370c8b51afb394094791e9
Smite Chow <xiaopengyou@live.com>
2016-05-27 13:59:44 +0800

    :art: improve compatibility to Django debug toolbar cache panel
    If direct import method from module will lose monkey-patch chance

--- cacheback/utils.py
+++ cacheback/utils.py
@@ -31,14 +31,13 @@ def get_cache(backend, **kwargs):
     original source:
     https://github.com/vstoykov/django-imagekit/commit/c26f8a0538778969a64ee471ce99b25a04865a8e
     """
-    try:
-        from django.core.cache import _create_cache
-    except ImportError:
-        # Django < 1.7
-        from django.core.cache import get_cache as _get_cache
-        return _get_cache(backend, **kwargs)
+    from django.core import cache
+    
+    # Django < 1.7
+    if not hasattr(cache, '_create_cache'):
+        return cache.get_cache(backend, **kwargs)
 
-    cache = _create_cache(backend, **kwargs)
+    cache = cache._create_cache(backend, **kwargs)
     # Some caches -- python-memcached in particular -- need to do a cleanup at the
     # end of a request cycle. If not implemented in a particular backend
     # cache.close is a no-op. Not available in Django 1.5
