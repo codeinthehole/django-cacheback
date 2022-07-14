0873ee5b2f673f68b77ab1d50dd4549dd884dcd4
Carlos Gottberg <13184659+el-chogo@users.noreply.github.com>
2022-07-14 03:10:10 -0400

    Modify test so it passes with current fix

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -66,8 +66,7 @@ class TestFunctionJob:
             'cache_alias': 'secondary',
         }
 
-    @pytest.mark.skipif(celery.VERSION[:2] < (4, 4), reason="will only work with the new kombu serializer")
-    def test_non_serializable_creates_two_entries(self, settings):
+    def test_non_serializable_creates_one_entry(self, settings):
         settings.CELERY_TASK_ALWAYS_EAGER = True
         settings.CACHEBACK_TASK_QUEUE = "celery"
 
@@ -85,10 +84,12 @@ class TestFunctionJob:
 
         assert empty_result is None
         assert dec_function.job.cache.has_key(sync_key)
-        assert dec_function.job.cache.has_key(async_key)
+        assert not dec_function.job.cache.has_key(async_key)
 
         result = dec_function(arg)
 
+        # use str_arg for dummy_function as celery serializes the UUID as a str
+        assert result == dummy_function(str_arg)
 
 @pytest.mark.django_db
 class TestQuerySetJob:
